a3884724b17ef3e749ebbfdc28630c0576da2b68
AntonYakovlev7119 <anton.vbg.02@yandex.ru>
2023-04-26 23:20:31 +0300

    1.5

--- js/app.js
+++ js/app.js
@@ -3,10 +3,9 @@ const nav_container = document.querySelector(".header-nav-container");
 window.onscroll = function () {
   let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
   if (scrollTop > 64) {
-    nav_container.classList.add("fixed");
+    nav_container.classList.add("scrolled");
   }
   if (scrollTop < 64) {
-    nav_container.classList.remove("fixed");
+    nav_container.classList.remove("scrolled");
   }
-  console.log(scrollTop);
 };
