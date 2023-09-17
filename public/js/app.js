5b1e80b5053098b27acea84e21dd1182afd675f3
AntonYakovlev7119 <anton.vbg.02@yandex.ru>
2023-09-17 22:54:16 +0300

    1.8.4

--- public/js/app.js
+++ public/js/app.js
@@ -20,8 +20,8 @@ toggle_menu.addEventListener("click", () => {
 document.addEventListener("mousemove", (e) => {
   Object.assign(document.documentElement, {
     style: `
-		--move-x: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
-		--move-y: ${(e.clientY - window.innerHeight / 2) * 0.01}deg;
+		--move-x: ${(e.clientX - window.innerWidth / 2) * -0.01}deg;
+		--move-y: ${(e.clientY - window.innerHeight / 2) * 0.015}deg;
 		`,
   });
 });
